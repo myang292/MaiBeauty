b03a5720f3476c89788adcba29b59a4ecbf9069d
Michelle Yang <myang292@gmail.com>
2022-05-09 21:46:46 -0700

    frontend installed and footer set

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './style/App.css';
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <Nav />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
