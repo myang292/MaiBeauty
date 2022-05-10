66e53bbe6fa30c8c0bd76dfb5962b9e194627cbb
Michelle Yang <myang292@gmail.com>
2022-05-09 22:33:25 -0700

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './style/App.css';
-import Nav from './components/Nav'
 import Footer from './components/Footer'
+import Home from './pages/Home'
+import Nav from './components/Nav'
+import Products from './pages/Products'
+import SignIn from './components/SignIn'
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <h1>
+        MaiBeauty
+      </h1>
       <Nav />
+      <SignIn />
       <Footer />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+        </Routes>
+      </main>
     </div>
   );
 }
