42e7ab662713c1bf4d53787e74251399c3a0ad31
Michelle Yang <myang292@gmail.com>
2022-05-14 15:22:23 -0500

    server updated

--- backend/server.js
+++ backend/server.js
@@ -14,34 +14,73 @@ app.get('/', (req, res) => {
     res.send('Okayyyyy')
 })
 
-app.get('/api/', async (req, res) => {
+app.get('/api/bronzer', async (req, res) => {
     const allPokemons = await Pokemon.find({});
-    res.send(allPokemons);
-    // res.json(allpokemons)
+    res.send(allBronzers);
 })
 
-app.get('/api/pokemons/:id', async (req, res) => {
+app.get('/api/bronzer/:id', async (req, res) => {
     const { id } = req.params;
     const onePokemon = await Pokemon.findById(id)
-    res.json(onePokemon)
+    res.json(oneBronzer)
 })
 
-app.get('/api/poketypes', async (req, res) => {
-        const allPokeTypes = await Poketype.find({})
-        res.send(allPokeTypes)
+app.get('/api/brow', async (req, res) => {
+    const allPokemons = await Pokemon.find({});
+    res.send(allBrows);
+})
+
+app.get('/api/brow/:id', async (req, res) => {
+    const { id } = req.params;
+    const onePokemon = await Pokemon.findById(id)
+    res.json(oneBrow)
+})
+
+app.get('/api/foundation', async (req, res) => {
+    const allPokemons = await Pokemon.find({});
+    res.send(allFoundations);
+})
+
+app.get('/api/foundation/:id', async (req, res) => {
+    const { id } = req.params;
+    const onePokemon = await Pokemon.findById(id)
+    res.json(oneFoundation)
+})
+
+app.get('/api/lipstick', async (req, res) => {
+    const allPokemons = await Pokemon.find({});
+    res.send(allLipsticks);
+})
+
+app.get('/api/lipstick/:id', async (req, res) => {
+    const { id } = req.params;
+    const onePokemon = await Pokemon.findById(id)
+    res.json(oneLipstick)
+})
+
+
+app.get('/api/makeup', async (req, res) => {
+        const allMakeup = await Makeup.find({})
+        res.send(allMakeup)
     })
+
+app.get('/api/makeup/:id', async (req, res) => {
+    const { id } = req.params;
+    const oneMakeup = await Makeup.findById(id)
+    res.json(oneMakeup)
+})
+
+
+
+
+
+// CRUD!!!!
 
 app.post('/api/add-pokemon', async (req, res) => {
         const addNewPokemon = await req.body
             Pokemon.insertMany(addNewPokemon)
             return res.status(201).json({Pokemon})
     })
-
-app.get('/api/poketypes/:id', async (req, res) => {
-    const { id } = req.params;
-    const onePoketype = await Poketype.findById(id)
-    res.json(onePoketype)
-})
 
 app.put('/api/pokemons/:id', async (req, res) => {
     const { num, name, img, type, height, weight, weakness } = await req.body;
